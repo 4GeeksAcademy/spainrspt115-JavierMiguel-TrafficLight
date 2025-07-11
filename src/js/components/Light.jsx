d05d6c4ff32a16bd2bfd980635650dcaa04b6b06
Jmvdev <jvi96miguel@gmail.com>
2025-07-11 19:56:49 +0000

    Second challenge completed, PD: srry for the code

--- src/js/components/Light.jsx
+++ src/js/components/Light.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import '../../styles/index.css';
 
-export const Light = ({ lightColor, idLight, selectedLight, onClick }) => {
+export const Light = ({ lightColor, idLight, selectedLight, onClick, style, display }) => {
   return (
-
     <button
       onClick={onClick}
-      className={`rounded-circle bg-${lightColor} ${selectedLight === idLight ? "selected" : ""}`}
-      style={{ width: '90px', height: '90px' }}
-    >
-    </button>
-    
+      className={`${display ? "" : "d-none"} rounded-circle ${selectedLight === idLight ? 'selected' : ''} ${lightColor === 'purple' ? '' : `bg-${lightColor}`}`}
+      style={{
+        width: '90px',
+        height: '90px',
+        ...(lightColor === 'purple' ? style : {})
+        
+      }}
+    ></button>
   );
 };
--- src/js/components/TrafficLight.jsx
+++ src/js/components/TrafficLight.jsx
@@ -3,6 +3,7 @@ import { Light } from './Light';
 
 export const TrafficLight = () => {
   const [selectedLight, setSelectedLight] = useState(0);
+  const [showPurple, setShowPurple] = useState(false);
 
   console.log(selectedLight);
   
@@ -13,27 +14,44 @@ export const TrafficLight = () => {
 
         <Light 
           lightColor="danger" 
+          display={true}
           idLight={1} 
           selectedLight={selectedLight} 
           onClick={() => setSelectedLight(selectedLight === 1 ? 0 : 1)} 
         />
         <Light 
           lightColor="warning" 
+          display={true}
           idLight={2} 
           selectedLight={selectedLight} 
           onClick={() => setSelectedLight(selectedLight === 2 ? 0 : 2)} 
         />
         <Light 
-          lightColor="success" 
+          lightColor="success"
+          display={true}
           idLight={3} 
           selectedLight={selectedLight} 
           onClick={() => setSelectedLight(selectedLight === 3 ? 0 : 3)} 
         />
+        <Light 
+          lightColor="purple" 
+          display={showPurple}
+          idLight={4} 
+          selectedLight={selectedLight} 
+          onClick={() => setSelectedLight(selectedLight === 4 ? 0 : 4)}
+          style={{ backgroundColor: 'purple' }}
+        />
 
       </div>
         {/* button to change color *challenge 1* */}
-        <button onClick={() => setSelectedLight(selectedLight === 2 ? 3 : selectedLight === 1 ? 2 : 1)} className="mt-3">Change Light Color</button>
-
+        <button onClick={() => setSelectedLight(selectedLight === 3 && showPurple ? 4 : selectedLight === 2 ? 3 : selectedLight === 1 ? 2 : 1)} className="mt-3">
+          Change Light Color
+        </button>
+        {/* button to enable or disable purple light */}
+        <button onClick={() => {setShowPurple(!showPurple); setSelectedLight(showPurple && selectedLight === 4 ? 1 : !showPurple && selectedLight != 4 ? 4 : selectedLight)}} 
+          className="mt-3">
+          {showPurple ? 'Disable' : 'Enable'} Purple Light
+        </button>
     </div>
   );
 };
